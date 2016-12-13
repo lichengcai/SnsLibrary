4518b06a78f0a97be484c4f57f8d4aa59c96dc83
lichengcai <lichengcai@koolearn.com>
2016-12-13 14:51:38 +0800

    test

---
+++ snslibrary/src/main/java/com/snslibrary/Test.java
@@ -0,0 +1,13 @@
+package com.snslibrary;
+
+import android.util.Log;
+
+/**
+ * Created by lichengcai on 2016/12/13.
+ */
+
+public class Test {
+    public void test() {
+        Log.d("test","it is just a test");
+    }
+}
